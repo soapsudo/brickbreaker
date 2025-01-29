b18d68d7371f76a7f69e2f9f7f6c4e0511318c41
BlazOstojic <blaz.ostoji22@gmail.com>
2025-01-29 20:36:13 +0100

    [.gitignore] Create gitignore and refactor the frame initalization.

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    private Main(){
+        super("BrickBreaker");
+
+        setTitle("BrickBreaker");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(800, 600);
+    }
+
+    static class DrawPane extends JPanel{
+        public void paintComponent(Graphics g){
+            
+        }
+    }
 
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        Main main = new Main();
+        main.frameInit();
+    }
+
+    private static void loop(JFrame frame, JPanel panel){
+        while(true){
+
+        }
+
     }
 }
